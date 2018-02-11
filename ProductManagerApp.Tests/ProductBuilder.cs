9b0c8e21f38d4bd51927d96597c2ee55a8b4b78e
BrankoMilosavljevic <banemi89@gmail.com>
2018-02-11 13:19:22 +0100

    Refactoring: Introducing unit tests
    Testing what matters, domain logic (query extension)

---
+++ ProductManagerApp.Tests/ProductBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ProductManagerApp.Domain;
+
+namespace ProductManagerApp.Tests
+{
+    public class ProductBuilder
+    {
+        private int _id = 1;
+        private string _name = "Clean code";
+        private string _photo = "C:/Images/Books/CleanCode.jpg";
+        private double _price = 60;
+        private DateTime _lastUpdated = DateTime.Now;
+
+        public ProductBuilder WithId(int id)
+        {
+            _id = id;
+            return this;
+        }
+        public ProductBuilder WithName(string name)
+        {
+            _name = name;
+            return this;
+        }
+        public ProductBuilder WithPhoto(string photo)
+        {
+            _photo = photo;
+            return this;
+        }
+        public ProductBuilder WithPrice(double price)
+        {
+            _price = price;
+            return this;
+        }
+        public ProductBuilder LastUpdated(DateTime lastUpdated)
+        {
+            _lastUpdated = lastUpdated;
+            return this;
+        }
+
+        public Product Build()
+        {
+            return new Product
+            {
+                Id = _id,
+                Name = _name,
+                Photo = _photo,
+                Price = _price,
+                LastUpdated = _lastUpdated
+            };
+        }
+
+        public static implicit operator Product(ProductBuilder builder)
+        {
+            return builder.Build();
+        }
+    }
+}
---
+++ ProductManagerApp.Tests/ProductFixture.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using ProductManagerApp.Domain;
+using ProductManagerApp.Services;
+
+namespace ProductManagerApp.Tests
+{
+    [Category("unit")]
+    public class ProductFixture
+    {
+        [TestCase("TestBook", 60, 58, 62, 1, TestName = "Regular case")]
+        [TestCase("TestBook", 60, 61, 62, 0, TestName = "Price from too high")]
+        [TestCase("TestBook", 60, 58, 59, 0, TestName = "Price to too low")]
+        public void ProductQueryExtension_Test(string name, double actualPrice, double priceFrom, double priceTo, int expectedCount)
+        {
+            Product product = new ProductBuilder()
+                                .WithName(name)
+                                .WithPrice(actualPrice);
+
+            IQueryable<Product> query = Enumerable.Repeat(product, 1).AsQueryable();
+            var result = query.WhereFilterIsSatisfied(name, priceFrom, priceTo);
+
+            result.Should().HaveCount(expectedCount);
+        }
+    }
+}
